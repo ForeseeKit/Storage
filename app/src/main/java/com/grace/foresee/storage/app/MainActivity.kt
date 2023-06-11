2a8d7ca6478f58bf2f571b459776084f8a0879b9
grace <graceens@163.com>
2023-06-11 14:18:24 +0800

    修改远程仓库地址

--- app/src/main/java/com/grace/foresee/storage/app/MainActivity.kt
+++ app/src/main/java/com/grace/foresee/storage/app/MainActivity.kt
@@ -17,18 +17,106 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val filename = "hello.txt"
         val content = "Hello world!"
 
-        StorageWriter.write(getAppCachePath(filename), content)
-        StorageWriter.write(getAppFilesPath(filename), content)
+        // Read or write for cache
+        "test.txt".also { filename ->
+            StorageWriter.write(getAppCachePath(filename), content)
+            StorageWriter.write(getAppFilesPath(filename), content)
 
-        Log.i(TAG, "cache content: ${StorageReader.read(getAppCachePath(filename))}")
-        Log.i(TAG, "files content: ${StorageReader.read(getAppFilesPath(filename))}")
+            Log.i(TAG, "from cache: ${StorageReader.read(getAppCachePath(filename))}")
+            Log.i(TAG, "from files: ${StorageReader.read(getAppFilesPath(filename))}")
+        }
 
-        MediaStoreWriter.write(this, MediaType.DOWNLOADS, "text/plain", filename = filename, content = content)
+        // Read or write for media store
+        // For Downloads
+        "test.txt".also { filename ->
+            MediaStoreWriter.write(
+                this,
+                MediaType.DOWNLOADS,
+                "text/plain",
+                filename = filename,
+                content = content
+            )
+            Log.i(
+                TAG,
+                "from Downloads: ${
+                    MediaStoreReader.read(
+                        this,
+                        MediaType.DOWNLOADS,
+                        filename = filename
+                    )
+                }"
+            )
+        }
 
-        Log.i(TAG, "media content: ${MediaStoreReader.read(this, MediaType.DOWNLOADS, filename = filename)}")
+        // For Documents
+        "test.txt".also { filename ->
+            MediaStoreWriter.write(
+                this,
+                MediaType.DOCUMENTS,
+                "text/plain",
+                filename = filename,
+                content = content
+            )
+            Log.i(
+                TAG,
+                "from Documents: ${
+                    MediaStoreReader.read(
+                        this,
+                        MediaType.DOCUMENTS,
+                        filename = filename
+                    )
+                }"
+            )
+        }
+
+        // For Images
+        "test.png".also { filename ->
+            MediaStoreWriter.write(
+                this,
+                MediaType.IMAGES,
+                "image/*",
+                filename = filename,
+                content = content
+            )
+            Log.i(
+                TAG,
+                "from Images: ${MediaStoreReader.read(this, MediaType.IMAGES, filename = filename)}"
+            )
+        }
+
+
+        // For Video
+        "test.mp4".also { filename ->
+            MediaStoreWriter.write(
+                this,
+                MediaType.VIDEO,
+                "video/mp4",
+                filename = filename,
+                content = content
+            )
+            Log.i(
+                TAG,
+                "from Video: ${MediaStoreReader.read(this, MediaType.VIDEO, filename = filename)}"
+            )
+        }
+
+        // For Audio
+        "test.mp3".also { filename ->
+            MediaStoreWriter.write(
+                this,
+                MediaType.AUDIO,
+                "audio/mp3",
+                filename = filename,
+                content = content
+            )
+            Log.i(
+                TAG,
+                "from Audio: ${MediaStoreReader.read(this, MediaType.AUDIO, filename = filename)}"
+            )
+        }
+
     }
 
     companion object {
